30c5fce6aa4446b9b963650181ed5312508588ff
Bilalik369 <bilal11iken@gmail.com>
2025-05-03 19:37:54 +0100

    Ajout de la route d'inscription avec vérification et génération de token JWT

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs"
 
 
 const userSchema = new mongoose.Schema({
@@ -33,6 +34,17 @@ const userSchema = new mongoose.Schema({
 
 
 });
+
+//hash password  before save user to db
+userSchema.pre("save" , async function( next) {
+
+    if(!this.isModified("password") ) 
+        return next()
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+    
+})
 
 const User = mongoose.model("User" , userSchema);
 
