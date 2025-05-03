7ddb73d956b1dc510335717144be02ec5bb87fa8
Bilalik369 <bilal11iken@gmail.com>
2025-05-03 18:10:53 +0100

    Ajout du modèle d'utilisateur avec schema Mongoose

--- backend/src/index.js
+++ backend/src/index.js
@@ -14,6 +14,6 @@ app.use(express.json());
 app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => {
-  console.log(`🚀 Server is running on port ${PORT}`);
+  console.log(` Server is running on port ${PORT}`);
   connectDB();
 });
