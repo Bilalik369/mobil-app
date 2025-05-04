adfbbc82154ecb9bf9a5030016c11befff4e3b8d
Bilalik369 <bilal11iken@gmail.com>
2025-05-04 16:22:52 +0100

    Ajout de l'authentification utilisateur et configuration de Cloudinary

--- backend/src/routes/bookRoutes.js
+++ backend/src/routes/bookRoutes.js
@@ -1,15 +1,36 @@
 import express from "express";
+import cloudinary from "../lib/cloudinary.js";
+import Book from "../models/Book.js";
 
 const router = express.Router();
 
+router.post("/", protectRoute ,async (req, res) => {
+  try {
+    const { title, caption, rating, image } = req.body;
 
-router.get("/", (req, res) => {
-  res.json({ message: " " });
-});
+    if (!image || !title || !caption || !rating) {
+      return res.status(400).json({ msg: "Please provide all fields" });
+    }
+
+    const uploadResponse = await cloudinary.uploader.upload(image);
+    const imageUrl = uploadResponse.secure_url;
 
 
-router.post("/", (req, res) => {
-  res.json({ message: " " });
+    const newBook = new Book({
+      title,
+      caption,
+      rating,
+      image: imageUrl,
+      
+    });
+
+    await newBook.save();
+
+    res.status(201).json(newBook);
+  } catch (error) {
+    console.error("Error saving book:", error);
+    res.status(500).json({ msg: "Server error" });
+  }
 });
 
 export default router;
