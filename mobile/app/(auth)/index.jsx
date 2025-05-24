135f3f5bb4f25a9258a8e685d35dba94f1db0eab
Bilalik369 <bilal11iken@gmail.com>
2025-05-24 17:26:15 +0100

    ajoute qulque chose

--- mobile/app/(auth)/index.jsx
+++ mobile/app/(auth)/index.jsx
@@ -1,106 +1,133 @@
-  import { Text, TextInput, View, TouchableOpacity } from "react-native";
-  import styles from "../../assets/styles/login.styles";
-  import { useState } from "react";
-  import { Image } from "react-native";
-  import {Ionicons}  from "@expo/vector-icons"
-import COLORS from "../../constants/colors";
+    import { Text, TextInput, View, TouchableOpacity, ActivityIndicator, KeyboardAvoidingView, Platform } from "react-native";
+    import styles from "../../assets/styles/login.styles";
+    import { useState } from "react";
+    import { Image } from "react-native";
+    import {Ionicons}  from "@expo/vector-icons"
+  import COLORS from "../../constants/colors";
+  import { Link } from "expo-router";
 
 
 
 
-  export default function Login() {
-    const [email , setEmail] = useState('');
-    const [password , setPassword] = useState('');
-    const [showpassword , setShowPassword] = useState(false);
+    export default function Login() {
+      const [email , setEmail] = useState('');
+      const [password , setPassword] = useState('');
+      const [showpassword , setShowPassword] = useState(false);
 
-    const [isLoading , setIsLoading] = useState(false);
+      const [isLoading , setIsLoading] = useState(false);
 
 
 
-    const handlelogin = () =>{
+      const handlelogin = () =>{
 
-    }
-    return (
-      <View style={styles.container}>
-        <View style= {styles.topIllustration}>
-          <Image
-          source={require("../../assets/images/i.png")}
-          style = {styles.illustrationImage}
-          resizeMode="contain"/>
+      }
+      return (
+        <KeyboardAvoidingView
+        style ={{flex: 1}}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        >
+        <View style={styles.container}>
+          <View style= {styles.topIllustration}>
+            <Image
+            source={require("../../assets/images/i.png")}
+            style = {styles.illustrationImage}
+            resizeMode="contain"/>
 
-        </View>
-        <View style={styles.card}>
-        <View style={styles.formContainer}>
-          {/* emal */}
-          <View style={styles.inputGroup}>
-               
-            <Text style={styles.label}>Email</Text>
-            <View style={styles.inputContainer}>
-              <Ionicons 
-              name="mail-outline"
-              size={20}
-              color={COLORS.primary}
-              style ={styles.inputIcon}/>
-
-              <TextInput
-              style ={styles.input}
-              placeholder="Entre your email"
-              placeholderTextColor={COLORS.placeholderText}
-              value={email}
-              onChangeText={setEmail}
-              keyboardType="email-address"
-
-              autoCapitalize="none"
-
-              />
-                 
-               
-
-            </View>
-            
           </View>
-
-          <View> 
-            {/* psrd */}
-            <View style={styles.inputGroup}
-            >
-              <Text style={styles.label}>Password</Text>
+          <View style={styles.card}>
+          <View style={styles.formContainer}>
+            {/* emal */}
+            <View style={styles.inputGroup}>
+                
+              <Text style={styles.label}>Email</Text>
               <View style={styles.inputContainer}>
-
-                <Ionicons
-                name="lock-closed-outline"
+                <Ionicons 
+                name="mail-outline"
                 size={20}
                 color={COLORS.primary}
-                style={styles.inputIcon}
-                />
+                style ={styles.inputIcon}/>
+
                 <TextInput
-                style={styles.input}
-                placeholder="Entre your password"
+                style ={styles.input}
+                placeholder="Entre your email"
                 placeholderTextColor={COLORS.placeholderText}
-                value={password}
-                onChangeText={setPassword}
-                secureTextEntry={!showpassword}
+                value={email}
+                onChangeText={setEmail}
+                keyboardType="email-address"
+
+                autoCapitalize="none"
+
                 />
-                <TouchableOpacity
-                onPress={() => setShowPassword(!showpassword)}
-                style={styles.eyeIcon}>
-                  <Ionicons
-                  name={showpassword ? "eye-outline" : "eye-off-outline"}
-                  size={20}
-                  color={COLORS.primary} />
-                </TouchableOpacity>
+                  
+                
+
               </View>
+              
             </View>
+
+            
+              {/* psrd */}
+              <View style={styles.inputGroup}
+              >
+                <Text style={styles.label}>Password</Text>
+                <View style={styles.inputContainer}>
+
+                  <Ionicons
+                  name="lock-closed-outline"
+                  size={20}
+                  color={COLORS.primary}
+                  style={styles.inputIcon}
+                  />
+                  <TextInput
+                  style={styles.input}
+                  placeholder="Entre your password"
+                  placeholderTextColor={COLORS.placeholderText}
+                  value={password}
+                  onChangeText={setPassword}
+                  secureTextEntry={!showpassword}
+                  />
+                  <TouchableOpacity
+                  onPress={() => setShowPassword(!showpassword)}
+                  style={styles.eyeIcon}>
+                    <Ionicons
+                    name={showpassword ? "eye-outline" : "eye-off-outline"}
+                    size={20}
+                    color={COLORS.primary} />
+                  </TouchableOpacity>
+                </View>
+              </View>
+              <TouchableOpacity style={styles.button} onPress={handlelogin}
+              disabled ={isLoading}>
+                {
+                  isLoading ? (
+                    <ActivityIndicator color="#fff"/>
+                  ) : (
+                    <Text style={styles.buttonText}>Login</Text>
+                  )
+                }
+
+              </TouchableOpacity>
+
+              <View style={styles.footer}>
+                <Text style={styles.footerText}>Don t have an account</Text>
+                <Link href= "/signup" asChild>
+                <TouchableOpacity>
+                  <Text style={styles.link}>Sign up</Text>
+                </TouchableOpacity>
+                </Link>
+
+              </View>
+  
             
 
+
+          
+
           </View>
-
-         
-
+          </View>
+          
+          
         </View>
-        </View>
-        
-        
-      </View>
-    );
-  }
+        </KeyboardAvoidingView>
+      );
+    }
